41e07b9a8a8ef3547a5f8cb3842549c90dc43f7d
pelev1n-dev <pelev1n.dev@gmail.com>
2021-05-31 23:43:32 +0300

    Complete lesson_3

--- script.js
+++ script.js
@@ -1,18 +1,35 @@
-let num = 266219;
+'use strict'
 
-let arrNumber = num.toString().split('');
-console.log(arrNumber);
+let lang = prompt("Введите язык 'ru' или 'en'");
 
-let multiDigital = 1;
-arrNumber.forEach(function (init){
-  multiDigital *= init;
-});
-console.log(multiDigital);
+ if (lang === 'ru') {
+   console.log('понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье');
+ } else if (lang === 'en') {
+   console.log('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday');
+ } else {
+   console.log("Введен язык не 'ru' и не 'en'");
+ }
 
-let multiDigitalPow = multiDigital ** 3
-console.log(multiDigitalPow);
+ switch (lang) {
+   case 'ru': console.log('понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье')
+     break;
+  case 'en': console.log('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')
+     break;
+  default: console.log("Введен язык не 'ru' и не 'en'")
+}
 
-console.log(+(multiDigitalPow.toString().substr(0,2)));
+let arrLang = [];
 
-const reducer = (accumulator, currentValue) => accumulator * currentValue;
-console.log(arrNumber.reduce(reducer));
+arrLang['ru'] = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
+arrLang['en'] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+console.log(arrLang[lang]);
+
+
+
+ let namePerson;
+ namePerson = 'Артем';
+ namePerson = (namePerson === 'Артем') ? console.log('директор') : (namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
+ namePerson = 'Максим';
+ namePerson = (namePerson === 'Артем') ? console.log('директор') : (namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
+ namePerson = 'Дмитрий';
+ namePerson = (namePerson === 'Артем') ? console.log('директор') : (namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
