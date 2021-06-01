d78de953fe7e48b6217c817f555b04323dc9fa64
pelev1n-dev <pelev1n.dev@gmail.com>
2021-06-01 23:20:11 +0300

    Complete lesson 4

--- script.js
+++ script.js
@@ -1,35 +1,20 @@
 'use strict'
-
-let lang = prompt("Введите язык 'ru' или 'en'");
-
- if (lang === 'ru') {
-   console.log('понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье');
- } else if (lang === 'en') {
-   console.log('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday');
- } else {
-   console.log("Введен язык не 'ru' и не 'en'");
- }
-
- switch (lang) {
-   case 'ru': console.log('понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье')
-     break;
-  case 'en': console.log('monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday')
-     break;
-  default: console.log("Введен язык не 'ru' и не 'en'")
-}
-
-let arrLang = [];
-
-arrLang['ru'] = ['понедельник', 'вторник', 'среда', 'четверг', 'пятница', 'суббота', 'воскресенье'];
-arrLang['en'] = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-console.log(arrLang[lang]);
-
-
-
- let namePerson;
- namePerson = 'Артем';
- namePerson = (namePerson === 'Артем') ? console.log('директор') : (namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
- namePerson = 'Максим';
- namePerson = (namePerson === 'Артем') ? console.log('директор') : (namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
- namePerson = 'Дмитрий';
- namePerson = (namePerson === 'Артем') ? console.log('директор') : (namePerson === 'Максим') ? console.log('преподаватель') : console.log('студент');
+let text1 = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ' +
+  'Donec eu nisi mi. Aenean leo lectus, maximus sit amet aliquet sit amet, luctus ac turpis. ' +
+  'Mauris laoreet efficitur vestibulum. Praesent vitae nunc nec tortor congue sagittis. ' +
+  'Sed porta elit et velit eleifend, et lobortis nisi tempus. ' +
+  'In aliquet nulla ut ante ullamcorper, eget rhoncus metus condimentum. ' +
+  'Nam magna velit, sodales a laoreet rutrum, porttitor vel diam.'
+let text2 = 'Lorem ipsum dolor sit amet';
+let text3 = 1112;
+const getSliceText = function (text) {
+  if (typeof text !== 'string' ) {
+    return alert('В аргумент передана не строка');
+  } else {
+    text = text.trim();
+    return text.length > 30 ? text.slice(0, 30) + '...' : text;
+  }
+};
+console.log(getSliceText(text1));
+console.log(getSliceText(text2));
+console.log(getSliceText(text3));
